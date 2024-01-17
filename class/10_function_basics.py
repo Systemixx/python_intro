64f6806fba1da6472227444c375c069386f9d768
Galina Atamankina <g.atamankina@gmail.com>
2024-01-17 13:34:30 +0100

    Adjusted comments in the examples.

--- class/10_function_basics.py
+++ class/10_function_basics.py
@@ -2,7 +2,7 @@
 def greet_world():
     print("Hello, World!")
 
-#greet_world()
+greet_world()
 
 # 2. Funktion - Definition mit einem Parameter
 def greet_name(name):
@@ -12,11 +12,11 @@ def greet_name(name):
         print("Argument ungueltig.")
 
 # Aufruf mit einem Argument
-# greet_name("Alice")
-# greet_name(3)
-# greet_name(["Alice", "Bob", "Lucy"])
-# new_name = "Bob"
-# greet_name(new_name)
+greet_name("Alice")
+greet_name(3)
+greet_name(["Alice", "Bob", "Lucy"])
+new_name = "Bob"
+greet_name(new_name)
 
 # 3. Funktion mit Rueckgabewert
 def calculate_sum(a, b):
@@ -31,47 +31,49 @@ def calculate_sum(a, b):
     return a + b
 
 # Den Rueckgabewert in einer Variable speichern
-# result = calculate_sum(5, 7)
-# print(result)
-# mult = result * 10
-# print(mult)
+result = calculate_sum(5, 7)
+print(result)
+mult = result * 10
+print(mult)
 
 # 4. Funktion mit Standardparametern
 def power(base, exponent = 2):
     return base ** exponent
 
-# result = power(3)
-# print(result)
-# result = power(3,3)
-# print(result)
-# result = power(2,8)
-# print(result)
+result = power(3)
+print(result)
+result = power(3,3)
+print(result)
+result = power(2,8)
+print(result)
 
 # 5. Funktion mit einer variablen Anzahl von Argumenten
 def average(*numbers):
     return sum(numbers) / len(numbers)
 
-# print(average(1,2,3,4))
-# print(average(10,20,30))
-# print(average(11,22,33,44,55,66,77,88,99))
+print(average(1,2,3,4))
+print(average(10,20,30))
+print(average(11,22,33,44,55,66,77,88,99))
 
 # 6. Funktion mit mehreren Rueckgabewerten
 def find_min_max(numbers):
     return min(numbers), max(numbers)
 
 minimum, maximum = find_min_max([1, 2, 3, 4, 5])
-#print(f"Minimum: {minimum}, Maximum: {maximum}")
+print(f"Minimum: {minimum}, Maximum: {maximum}")
 
 minimum, maximum = find_min_max([100, 2, 30, 4000, 555])
-#print(f"Minimum: {minimum}, Maximum: {maximum}")
+print(f"Minimum: {minimum}, Maximum: {maximum}")
 
 # 7. Funktion mit Schluesselwortargumenten
 def print_details(name, age, city):
     print(f"Name: {name}, Age: {age}, City: {city}")
 
-# print_details(name="Alice", age=30, city="New York")
-# print_details(city="Berlin", name="Bob", age=35)
+print_details(name="Alice", age=30, city="New York")
+print_details(city="Berlin", name="Bob", age=35)
 
+# 8. Funktion, die eine beliebige Anzahl von Argumenten 
+# und Schlüsselwortargumenten akzeptiert
 def flexible_function(*args, **kwargs):
     print(args)
     for arg in args:
